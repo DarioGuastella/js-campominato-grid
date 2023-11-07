205169b86c54e4732a94438315c80a1ebd720158
DarioGuastella <jimmyguastella@gmail.com>
2023-11-07 17:30:26 +0100

    feat: mostra griglia al click del play btn

--- js/main.js
+++ js/main.js
@@ -2,15 +2,20 @@
 
 let numCelle = 100;
 let grid = document.getElementById("container");
+const playBtn = document.getElementById("myBtn");
 
-for (let i = 1; i <= numCelle; i++) {
-    const cella = document.createElement("div");
-    cella.innerHTML = i;
-    cella.classList.add("square");
-    grid.appendChild(cella);
-
-    cella.addEventListener("click", function() {
-        console.log("Cella selezionata", i);
-        cella.classList.toggle("highlight");
-    });
-}
+playBtn.addEventListener("click", function() {
+    
+    
+    for (let i = 1; i <= numCelle; i++) {
+        const cella = document.createElement("div");
+        cella.innerHTML = i;
+        cella.classList.add("square");
+        grid.appendChild(cella);
+        
+        cella.addEventListener("click", function() {
+            console.log("Cella selezionata", i);
+            cella.classList.toggle("highlight");
+        });
+    }
+});
