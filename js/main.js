0de26f4bff473bd3d23476427f58f2a022e11205
DarioGuastella <jimmyguastella@gmail.com>
2023-11-07 18:22:15 +0100

    feat: aggiunge difficoltà

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-// Creo la griglia con N celle nel container
-
-let numCelle = 100;
+// PROVA DIFFICOLTA
+let difficoltà = document.getElementById("difficulty");
+difficoltà.addEventListener('change', function() {
+    console.log('Hai selezionato: ', difficoltà.value);
+  });
+  let numCelle = 0;
+  
 let grid = document.getElementById("container");
 const playBtn = document.getElementById("myBtn");
 
 playBtn.addEventListener("click", function() {
+    if (difficoltà.value == "Normale") {
+        numCelle = 81;    
+      } else if (difficoltà.value == "Difficile"){
+        numCelle = 49;
+      } else {
+        numCelle = 100;  
+      }
+      console.log(numCelle);
     
-    
+    console.log(difficulty);
     for (let i = 1; i <= numCelle; i++) {
         let cella = creaCella(i);
         grid.appendChild(cella);
